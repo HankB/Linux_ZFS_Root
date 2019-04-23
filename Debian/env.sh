c5d0274673acc15005cc5ffbb4bedd917aae379c
Hank Barta <hbarta@gmail.com>
2019-04-23 16:02:29 -0500

    accommodate bpool and organize more sensibly

--- Debian/env.sh
+++ Debian/env.sh
@@ -1,13 +1,21 @@
 # source this script to set these variables in the user environment
 # adjust to your needs
-set -u
-export DRIVE_ID=
+set -u  # treat unset vars as errors
 export ETHERNET=enp3s0
-export NEW_HOSTNAME=saugage
+export NEW_HOSTNAME=mlbry01
 export YOURUSERNAME=hbarta
-export POOLNAME=rpool
-export USE_EXISTING_PART=yes
-export USE_EXISTING_PART=no
+export ROOT_POOL_NAME=rpool
+export BOOT_POOL_NAME=bpool
+
+# Select the type of installation
+export USE_EXISTING_PART=  # yes|no
+
+# for USE_EXISTING_PART=yes
 
 export EFI_PART=
 export ROOT_PART=
+export BOOT_PART=
+
+# for USE_EXISTING_PART=no
+
+export DRIVE_ID=
