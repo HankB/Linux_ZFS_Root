a9022e725a68cd3dfa19f0d5dde425027a04b100
Hank Barta <hbarta@gmail.com>
2019-08-01 22:19:44 -0500

    DRIVE_ID to match test vm

--- Debian/testing/vbx01.whole.sh
+++ Debian/testing/vbx01.whole.sh
@@ -22,7 +22,7 @@ export BOOT_PART=
 
 # for INSTALL_TYPE=whole_disk
 
-export DRIVE_ID=ata-VBOX_HARDDISK_VBcd1d4854-e6190a19
+export DRIVE_ID=ata-VBOX_HARDDISK_VBfafda65f-8b22812e
 # specify only the last part of the path.
 # e,g `export DRIVE_ID=ata-SATA_SSD_18120612000764`
 
