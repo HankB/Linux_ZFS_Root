928034a7ae8eb148c112e5c5a43d6e15c4e7bc51
Hank Barta <hbarta@gmail.com>
2021-03-17 10:19:06 -0500

    predefined partition test w/ BIOS boot

---
+++ Debian/testing/part.std.no_enc.bios.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# previous line here only to tell shellcheck what shell we're using
+
+# `install_Debian_to_ZFS_root.sh` can source this script to set 
+# these variables to determine how the install is to proceed.
+# adjust to your needs
+
+# Unaltered, this is intended to run in a Virtualbox VM configured to boot 
+# using BIOS/MBR. It will create a UEFI partition anyway which could
+# be useful in the future.
+
+set -u  # treat unset vars as errors
+export ETHERNET="enp0s3"
+export NEW_HOSTNAME="vbx01"
+export YOURUSERNAME="hbarta"
+export ROOT_POOL_NAME="rpool"
+export BOOT_POOL_NAME="bpool"
+
+# LUKS encryption for 0.7 and native encryptoion for 0.8
+export ZFS_CRYPT="no"
+export LUKS_CRYPT="no"
+
+
+# Select the type of installation "whole_disk|use_partitions|use_pools"
+export INSTALL_TYPE="use_partitions"
+export BOOT_TYPE=BIOS
+
+# for INSTALL_TYPE="use_partitions", set the following variables
+# Specify the entire path
+# e.g. 'export EFI_PART=/dev/disk/by-id/ata-SATA_SSD_18120612000764-part2'
+
+export EFI_PART=
+export ROOT_PART=
+export BOOT_PART=
+
+# for INSTALL_TYPE=whole_disk, set the following variable
+# specify only the last part of the path.
+# e,g `export DRIVE_ID=ata-SATA_SSD_18120612000764`
+
+export DRIVE_ID="ata-VBOX_HARDDISK_VBfafda65f-8b22812e"
+
+# following works in Virtualbox VMs.
+DRIVE_ID=$(cd /dev/disk/by-id || exit 1; find . -name "*VBOX_HARDDISK*"| sed s/^..//|sort|head -1)
+export DRIVE_ID
+
+##### build the partitions for the test
+# clear the drive
+wipefs -a /dev/disk/by-id/"$DRIVE_ID" # useful if the drive already had ZFS pools
+sgdisk --zap-all /dev/disk/by-id/"$DRIVE_ID"
+
+# BIOS boot
+sgdisk -a1 -n1:24K:+1000K -t1:EF02 /dev/disk/by-id/"$DRIVE_ID"
+
+# EFI
+sgdisk -n2:1M:+512M -t2:EF00 /dev/disk/by-id/"$DRIVE_ID"
+export EFI_PART=/dev/disk/by-id/"$DRIVE_ID"-part2
+apt update
+apt install dosfstools
+mkdosfs -F 32 -s 1 -n EFI "$EFI_PART"
+
+# boot
+sgdisk -n3:0:+1024M -t3:BF01 /dev/disk/by-id/"$DRIVE_ID"
+export BOOT_PART=/dev/disk/by-id/"$DRIVE_ID"-part3
+
+# root
+sgdisk -n4:0:0 -t4:BF01 /dev/disk/by-id/"$DRIVE_ID"
+export ROOT_PART=/dev/disk/by-id/"$DRIVE_ID"-part4
+#####
+
